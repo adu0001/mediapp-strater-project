6014e8847c3cf21df63f75f9b78365cd5f26e55a
adarsh <adarshmohan42@gmail.com>
2021-02-25 23:00:40 +0530

    mediapp starter project

---
+++ src/main/java/com/gov/mediapp/MediappStarterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gov.mediapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediappStarterProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MediappStarterProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/gov/mediapp/MediappStarterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gov.mediapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediappStarterProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
